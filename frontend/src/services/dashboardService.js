0804de2f26f2f43302929cb68a0e059ed3dc8f72
arifalpagut <127036084+arifalpagut@users.noreply.github.com>
2026-05-20 03:29:24 +0300

    Delete frontend directory

--- frontend/src/services/dashboardService.js
+++
@@ -1,9 +0,0 @@
-import api from './api';
-
-export const dashboardService = {
-  getSummary: () => api.get('/dashboard/summary'),
-  getStats: () => api.get('/dashboard/stats'),
-  getRecentActivities: () => api.get('/dashboard/recent-activities'),
-};
-
-export default dashboardService;
