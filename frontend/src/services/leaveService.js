a30ffa4ec51263e900f9ba84564c5557aa85360e
arifalpagut <127036084+arifalpagut@users.noreply.github.com>
2026-05-20 13:54:53 +0300

    Delete frontend directory

--- frontend/src/services/leaveService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getLeaves = (params) => api.get("/leaves", { params });
-export const getLeave = (id) => api.get(`/leaves/${id}`);
-export const createLeave = (data) => api.post("/leaves", data);
-export const updateLeave = (id, data) => api.patch(`/leaves/${id}`, data);
--- frontend/src/services/performanceService.js
+++
@@ -1,6 +0,0 @@
-import api from "./api";
-
-export const getPerformanceReviews = (params) => api.get("/performance", { params });
-export const getPerformanceReview = (id) => api.get(`/performance/${id}`);
-export const createPerformanceReview = (data) => api.post("/performance", data);
-export const updatePerformanceReview = (id, data) => api.put(`/performance/${id}`, data);
