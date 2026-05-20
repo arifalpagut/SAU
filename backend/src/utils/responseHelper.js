60eb6fb046f6b021ba9018ee29d48bf39c0517ce
arifalpagut <127036084+arifalpagut@users.noreply.github.com>
2026-05-20 13:26:35 +0300

    Delete backend directory

--- backend/src/utils/responseHelper.js
+++
@@ -1,9 +0,0 @@
-function success(res, data = null, message = 'Basarili', statusCode = 200) {
-  return res.status(statusCode).json({
-    success: true,
-    message,
-    data
-  });
-}
-
-module.exports = { success };
