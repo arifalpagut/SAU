89ce4257b4b1c8fc09d0c61500d8aa3dd8f9cd2f
arifalpagut <127036084+arifalpagut@users.noreply.github.com>
2026-05-20 03:29:48 +0300

    Delete backend directory

--- backend/src/middlewares/rbac.middleware.js
+++
@@ -1,9 +0,0 @@
-const AppError = require('../utils/AppError');
-
-module.exports = function allowRoles(...roles) {
-  return function rbacMiddleware(req, res, next) {
-    if (!req.user) return next(new AppError('Kimlik dogrulama gerekli', 401));
-    if (!roles.includes(req.user.role)) return next(new AppError('Bu islem icin yetkiniz bulunmamaktadir', 403));
-    next();
-  };
-};
